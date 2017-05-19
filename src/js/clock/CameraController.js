302e59bdbea553007b835b774f39592ba03235c2
Yusuke Omi <ysk.omi0818@gmail.com>
2017-05-19 22:29:39 +0900

    CameraControllerのリファクタリング

--- src/js/clock/CameraController.js
+++ src/js/clock/CameraController.js
@@ -31,23 +31,30 @@ class CameraController {
       this.param.near,
       this.param.far
     );
-    this.core.position.y = Math.sin(this.angle.y) * this.param.r;
-    this.core.position.x = Math.cos(this.angle.y) * this.param.r * Math.sin(this.angle.x);
-    this.core.position.z = Math.cos(this.angle.y) * this.param.r * Math.cos(this.angle.x);
+
+    //ポジション設定
+    this.setPosition();
 
     this.core.lookAt(this.param.lookAt);
   }
   update(){
+    //ターゲットとなるアングルへ値を変化させる
     this.angle.x += (this.targetAngle.x - this.angle.x) * 0.09;
     this.angle.y += (this.targetAngle.y - this.angle.y) * 0.09;
     this.angle.z+= (this.targetAngle.z - this.angle.z) * 0.09;
 
-    this.core.position.y = Math.sin(this.angle.y) * this.param.r;
-    this.core.position.x = Math.cos(this.angle.y) * this.param.r * Math.sin(this.angle.x);
-    this.core.position.z = Math.cos(this.angle.y) * this.param.r * Math.cos(this.angle.x);
+    //ポジション設定
+    this.setPosition();
 
     this.core.lookAt(this.param.lookAt);
   }
+  //yの場所を求めて、その高さで水平に球を切ったときの円の半径を求めてx,zを求める
+  setPosition(){
+    this.core.position.y = Math.sin(this.angle.y) * this.param.r;
+    this.core.position.x = Math.cos(this.angle.y) * this.param.r * Math.sin(this.angle.x);
+    this.core.position.z = Math.cos(this.angle.y) * this.param.r * Math.cos(this.angle.x);
+  }
+  //移動したいアングルを設定
   setTargetAngle(x, y, z){
     this.targetAngle.x = x;
     this.targetAngle.y = y;
